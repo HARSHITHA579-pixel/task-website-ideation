4ef6da25147459c3e64e0004a46596bd7688f1a9
Aritra Ghosh <aritraghosh.ag2005@gmail.com>
2026-06-10 21:22:42 +0530

    Feat: Numbers & Strings in JS pt-02

---
+++ AritraGhoshWebDev/practice/JS_Basics/sandbox1.js
@@ -0,0 +1,55 @@
+let radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi);
+
+// math operators +, -, *, /, **, %
+
+console.log(10/2);
+
+let result = radius % 3;
+console.log(result);
+
+result = pi*radius**2;
+console.log(result);
+
+// unary operators ++, --
+
+// shorthand operators +=, -=, *=, /=
+
+// NaN - not a number
+// console.log(5/"hello");
+
+// template strings (or literal) - inject variables inside string without using concatenation
+const title = 'A Good Girl\'s Guide TO Murder';
+const author = 'Holly Jackson';
+const likes = 30;
+
+result = `The book called ${title} by ${author} has ${likes} likes.`;
+console.log(result);
+
+// html templates
+let html = `
+    <h2>${title}</h2>
+    <p>By ${author}</p>
+    <span>This book has ${likes} likes</span>
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
