a909a05f73bd8e11afbbd9cf5d418bf1366240ee
Aritra Ghosh <aritraghosh.ag2005@gmail.com>
2026-06-10 19:12:03 +0530

    Feat: Learning JS pt-01

---
+++ AritraGhoshWebDev/practice/JS_Basics/sandbox.js
@@ -0,0 +1,61 @@
+alert('hello world');
+console.log(133);
+
+let age = 25;
+let year = 2005;
+
+// log things to console
+/*console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+var score = 75;
+console.log(score);*/
+
+// strings
+console.log('hello world');
+
+let email = 'aritraghosh.ag2005@gmail.com';
+console.log(email);
+
+// string concatenation
+let firstName = 'Aritra';
+let lastName = 'Ghosh';
+let fullName = firstName+' '+lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+
+console.log(email.lastIndexOf('n'));
+
+//  position, to position
+console.log(email.slice(0, 6));
+
+//  position, number of characters
+console.log(email.substr(0, 10));
+
+// first occurence
+console.log(email.replace('m', 'w'));
+
+
+
+
+
+
+
+
