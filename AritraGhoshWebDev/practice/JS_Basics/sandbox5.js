3263dbcf0165d5d5448180535eee46559e94535e
Aritra Ghosh <aritraghosh.ag2005@gmail.com>
2026-06-15 14:11:23 +0530

    Contd: Math functions

--- AritraGhoshWebDev/practice/JS_Basics/sandbox5.js
+++ AritraGhoshWebDev/practice/JS_Basics/sandbox5.js
@@ -44,7 +44,12 @@ const area = 7.7;
 console.log(Math.round(area));
 console.log(Math.floor(area));
 console.log(Math.ceil(area));
-console.log(Math.trunc(area));
+console.log(Math.trunc(area)); 
+
+// random numbers
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random * 100)); // random number between 0 and 100
 
 
 
