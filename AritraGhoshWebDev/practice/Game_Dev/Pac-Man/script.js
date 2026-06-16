9fd53db137e3a3370f560068bd7ef8cfdef572e7
Aritra Ghosh <aritraghosh.ag2005@gmail.com>
2026-06-16 13:07:12 +0530

    Feat: Pac-Man - Setting up files and initial canvas

---
+++ AritraGhoshWebDev/practice/Game_Dev/Pac-Man/script.js
@@ -0,0 +1,10 @@
+// board - 21 rows x 19 cols 
+// each tile 32px x 32px
+// height = 21 x 32 = 
+// width = 19 x 32 =
+let board;
+const rowCount = 21;
+const colCount = 19;
+const tileSize = 32;
+const boardWidth = colCount*tileSize;
+const boardHeight = rowCount*tileSize;
