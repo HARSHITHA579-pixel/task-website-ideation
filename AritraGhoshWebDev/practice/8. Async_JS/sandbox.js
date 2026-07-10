7d1557fae331559450e6f54ee33ac3c0a929931e
Aritra Ghosh <aritraghosh.ag2005@gmail.com>
2026-07-10 23:22:32 +0530

    Feat: Async JS HTTP Requests pt-02

--- AritraGhoshWebDev/practice/8. Async_JS/sandbox.js
+++ AritraGhoshWebDev/practice/8. Async_JS/sandbox.js
@@ -1,9 +1,26 @@
-console.log(1);
-console.log(2);
+// console.log(1);
+// console.log(2);
 
-setTimeout(() => {
-    console.log('callback function fired');
-}, 2000);
+// setTimeout(() => {
+//     console.log('callback function fired');
+// }, 2000);
 
-console.log(3);
-console.log(4);
+// console.log(3);
+// console.log(4);
+
+const request = new XMLHttpRequest(); // XML => older data format used much before JSON arrived
+
+// track the progress of request
+request.addEventListener('readystatechange', () => {
+    // console.log(request, request.readyState);
+
+    if(request.readyState === 4) {
+        console.log(request.responseText);
+    }
+
+
+}); // fires everytime the state change in the request - 4 different phases of request
+
+// We don't know when the request is complete
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/'); // setting up the request --- type of request, end-point
+request.send();
