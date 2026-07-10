d761a611e8bbb6e16c144c6a714a05315bf6f3c4
Aritra Ghosh <aritraghosh.ag2005@gmail.com>
2026-07-10 23:43:32 +0530

    Feat: Async JS callback funcs pt-04

--- AritraGhoshWebDev/practice/8. Async_JS/sandbox.js
+++ AritraGhoshWebDev/practice/8. Async_JS/sandbox.js
@@ -7,23 +7,49 @@
 
 // console.log(3);
 // console.log(4);
+// making it reusable
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest(); // XML => older data format used much before JSON arrived
 
-const request = new XMLHttpRequest(); // XML => older data format used much before JSON arrived
+    // track the progress of request
+    request.addEventListener('readystatechange', () => {
+        // console.log(request, request.readyState);
 
-// track the progress of request
-request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState);
+        if(request.readyState === 4 && request.status === 200) { // 4 => request is complete, 200 => success
+            // console.log(request.responseText);
+            callback(undefined, request.responseText);
+        }
+        else if(request.readyState === 4) {
+            // console.log('could not fetch the data');
+            callback('could not fetch data', undefined);
+        }
 
-    if(request.readyState === 4 && request.status === 200) { // 4 => request is complete, 200 => success
-        console.log(request.responseText);
+
+    }); // fires everytime the state change in the request - 4 different phases of request
+
+    // We don't know when the request is complete
+    // https://jsonplaceholder.typicode.com/todos/ - gives fake data
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/'); // setting up the request --- type of request, end-point
+    request.send();
+}
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+
+//convention - error, data
+getTodos((err, data) => {
+    console.log('callback fired');
+    if(err) {
+        console.log(err);
     }
-    else if(request.readyState === 4) {
-        console.log('could not fetch the data');
+    else {
+        console.log(data);
     }
+});
 
-
-}); // fires everytime the state change in the request - 4 different phases of request
-
-// We don't know when the request is complete
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/'); // setting up the request --- type of request, end-point
-request.send();
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
