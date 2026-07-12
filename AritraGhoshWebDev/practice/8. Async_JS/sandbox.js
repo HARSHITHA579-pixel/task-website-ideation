2260034908c7b94eaadf5bd625a6cbba30b93049
Aritra Ghosh <aritraghosh.ag2005@gmail.com>
2026-07-12 19:52:23 +0530

    Feat: Async JS chaining promises pt-08

--- AritraGhoshWebDev/practice/8. Async_JS/sandbox.js
+++ AritraGhoshWebDev/practice/8. Async_JS/sandbox.js
@@ -45,10 +45,16 @@ const getTodos = (resource, callback) => {
 };
 
 getTodos('todos/luigi.json').then(data => {
-    console.log('promise resolved: ', data);
+    console.log('promise 1 resolved: ', data);
+    return getTodos('todos/mario.json');
+}).then(data => {
+    console.log('promise 2 resolved', data);
+    return getTodos('todos/shaun.json');
+}).then(data => {
+    console.log('promise 3 resolved', data);
 }).catch(err => {
     console.log('promise rejected: ', err);
-});
+}); // only 1 catch required to handle all the chaining promises
 
 console.log(1);
 console.log(2);
