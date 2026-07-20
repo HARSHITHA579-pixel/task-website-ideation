b580438e970a98ed12a53eb27b13adaec1c5c386
Aritra Ghosh <aritraghosh.ag2005@gmail.com>
2026-07-20 18:22:24 +0530

    Feat: Learning React - setup, components, templates & dynamic values

---
+++ AritraGhoshWebDev/practice/10. React_Basics/dojo-blog/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  // this is not HTML, this is JSX. A compiler - Babel converts JSX to HTML. JSX is a syntax extension for JavaScript that looks similar to HTML. It allows you to write HTML-like code within your JavaScript code, making it easier to create and manage UI components in React.
+
+  const title = "Welcome to React";
+  // React converts everything into string to output it, except booleans & objects
+  const likes = 50;
+  const link = "https://www.google.com";
+
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{10}</p>
+        <p>{"Hello ninjas"}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{Math.random()*10}</p>
+
+        <a href={link}>Google</a>
+      </div>
+    </div>
+  );
+}
+// a component in nutshell is a function that returns JSX. This function is exported and can be imported in files such as index.js where it is rendered to the DOM by ReactDOM library.
+export default App;
---
+++ AritraGhoshWebDev/practice/10. React_Basics/dojo-blog/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // React does additional checks during development & gives us warning in the console if there are any potential problems in our app. It does not affect the production build.
+  <React.StrictMode> 
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
