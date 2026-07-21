f2e7ef66a457c8512c4954ccb3934d23f082386c
Aritra Ghosh <aritraghosh.ag2005@gmail.com>
2026-07-21 12:29:37 +0530

    Feat: click events in react

--- AritraGhoshWebDev/practice/10. React_Basics/dojo-blog/src/Navbar.js
+++ AritraGhoshWebDev/practice/10. React_Basics/dojo-blog/src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
             <h1>The Dojo Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                {/* JSX -> JS object */}
+                {/* Dynamic value -> JS object */}
                 <a href="/create" style={{
                     color: "white",
                     backgroundColor: '#f1356d',
